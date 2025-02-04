6af771f513f68a25a9d9d78229736ef0a7b90c11
m4rn-prog <purgeinflame321@gmail.com>
2025-02-03 21:28:32 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use crossterm::terminal;
+use std::fs;
+use std::io;
+
+enum Mode {
+    Sun,
+    Moon,
+    Cast,
+    Sky,
+    Star,
+}
+
+enum Actions {
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    Backspace,
+    Return,
+    Esc,
+    CtrlC,
+    CtrlV,
+}
+
+fn main() {
+    
+}
