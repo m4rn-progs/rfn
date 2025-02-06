4a1b3680ee0b7fb859ba9fb7d424417f07e59f66
m4rn-prog <purgeinflame321@gmail.com>
2025-02-06 09:05:40 -0500

    made an editor.rs file for some reason

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use crossterm::terminal;
+use anyhow;
+use crossterm::{cursor, event, execute, terminal, ExecutableCommand};
 use std::fs;
-use std::io;
+use std::io::{stdin, stdout, Read, Stdin, Write};
+mod editor;
 
 enum Mode {
     Sun,
@@ -15,13 +17,33 @@ enum Actions {
     MoveDown,
     MoveLeft,
     MoveRight,
-    Backspace,
-    Return,
-    Esc,
-    CtrlC,
-    CtrlV,
 }
 
-fn main() {
-    
+fn main() -> anyhow::Result<()> {
+    let mut cx: u16 = 0;
+    let mut cy: u16 = 0;
+    let mut mode = Mode::Sun;
+    startup();
+
+    shutdown();
+    Ok(())
+}
+
+fn startup() {
+    terminal::enable_raw_mode()?;
+    execute!(stdout(), terminal::EnterAlternateScreen)?;
+    execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
+    stdout().execute(cursor::MoveTo(cx, cy));
+
+    //      for b in stdin().bytes() {
+    //            let c = b.unwrap() as char;
+    //            println!("{}\r", c);
+    //            if c == 'q' {
+    //                break;
+    //            }
+    //        }
+}
+fn shutdown() {
+    execute!(stdout(), terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
 }
