5ad3b1a76dbdb44af842593a607f42a04c9b7076
m4rn-prog <purgeinflame321@gmail.com>
2025-02-09 13:19:39 -0500

    so like, I removed the editor.rs and added a whole bunch fo stuff and now I can show text from a file. Still can't scroll or navigate. But I'll figure it out soon enough.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use anyhow;
 use crossterm::{cursor, event, execute, terminal, ExecutableCommand};
+use ropey::Rope;
+use std::env::args_os;
+use std::ffi::OsString;
 use std::fs;
-use std::io::{stdin, stdout, Read, Stdin, Write};
-mod editor;
+use std::io::{stdin, stdout, BufReader, BufWriter, Read, Stdin, Write};
+use std::thread;
 
 enum Mode {
     Sun,
@@ -20,30 +23,36 @@ enum Actions {
 }
 
 fn main() -> anyhow::Result<()> {
+    let args: Vec<OsString> = args_os().collect();
+    let mut file_name: &OsString = &args[1];
     let mut cx: u16 = 0;
     let mut cy: u16 = 0;
     let mut mode = Mode::Sun;
-    startup();
+    terminal::enable_raw_mode()?;
+    execute!(stdout(), terminal::EnterAlternateScreen)?;
+    execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
+    stdout().execute(cursor::MoveTo(cx, cy));
+    let scrheight = terminal::size()?.1;
+    let mut file = Rope::from_reader(BufReader::new(fs::File::open(file_name)?))?;
+    for current_row in 0..scrheight {
+        if <u16 as Into<usize>>::into(current_row) < file.len_lines() {
+            print!("{}\r\n", file.line(current_row.into()));
+        } else {
+            print!("~\r\n");
+        }
+    }
+    for b in stdin().bytes() {
+        let c = b.unwrap() as char;
+        if c == 'q' {
+            break;
+        }
+    }
 
     shutdown();
     Ok(())
 }
 
-fn startup() {
-    terminal::enable_raw_mode()?;
-    execute!(stdout(), terminal::EnterAlternateScreen)?;
-    execute!(stdout(), terminal::Clear(terminal::ClearType::All))?;
-    stdout().execute(cursor::MoveTo(cx, cy));
-
-    //      for b in stdin().bytes() {
-    //            let c = b.unwrap() as char;
-    //            println!("{}\r", c);
-    //            if c == 'q' {
-    //                break;
-    //            }
-    //        }
-}
 fn shutdown() {
-    execute!(stdout(), terminal::LeaveAlternateScreen)?;
-    terminal::disable_raw_mode()?;
+    execute!(stdout(), terminal::LeaveAlternateScreen);
+    terminal::disable_raw_mode();
 }
