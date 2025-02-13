3a8b8ef48acb31c382a40830926c44620b1daa39
m4rn-prog <purgeinflame321@gmail.com>
2025-02-13 18:57:29 -0500

    Imma just save this rq

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use anyhow;
 use crossterm::{cursor, event, execute, terminal, ExecutableCommand};
+use ratatui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style},
+    text::{Line, Span},
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
 use ropey::Rope;
 use std::env::args_os;
 use std::ffi::OsString;
 use std::fs;
 use std::io::{stdin, stdout, BufReader, BufWriter, Read, Stdin, Write};
-use std::thread;
-
 enum Mode {
     Sun,
     Moon,
@@ -35,41 +41,56 @@ fn main() -> anyhow::Result<()> {
     execute!(stdout(), cursor::MoveTo(0, 0)).unwrap();
     let scrheight: usize = terminal::size()?.1.into();
     let mut file = Rope::from_reader(BufReader::new(fs::File::open(file_name)?))?;
+    draw(&mut file, scroll_offset, scrheight, cx, cy);
     for b in stdin().bytes() {
         let c = b.unwrap() as char;
         match c {
             'q' => break,
             'k' => {
                 if cy > 0 {
+                    execute!(stdout(), cursor::MoveUp(1)).unwrap();
                     cy -= 1;
                 } else if scroll_offset > 0 {
                     scroll_offset -= 1;
+                    draw(&mut file, scroll_offset, scrheight, cx, cy);
+                    stdout().flush().unwrap();
                 }
             }
             'j' => {
-                if cy > 0 {
-                    cy += 1;
-                } else if scrheight + scroll_offset < file.len_lines() {
-                    scroll_offset += 1;
+                if file.len_lines() < scrheight {
+                    if usize::from(cy) > file.len_lines().saturating_sub(1) {
+                        execute!(stdout(), cursor::MoveDown(1)).unwrap();
+                        cy += 1;
+                    }
+                } else {
+                    if usize::from(cy) >= scrheight {
+                        execute!(stdout(), cursor::MoveDown(1)).unwrap();
+                        cy += 1;
+                    } else if scrheight + scroll_offset < file.len_lines() {
+                        scroll_offset += 1;
+                        draw(&mut file, scroll_offset, scrheight, cx, cy);
+                        stdout().flush().unwrap();
+                    }
                 }
             }
             _ => {}
         }
-        draw(&file, scroll_offset, scrheight);
+        //draw(&mut file, scroll_offset, scrheight, cx, cy);
         stdout().flush().unwrap();
     }
     shutdown();
     Ok(())
 }
 
-fn draw(file: &Rope, scroll_offset: usize, scrheight: usize) {
+fn draw(file: &mut Rope, scroll_offset: usize, scrheight: usize, cx: u16, cy: u16) {
     execute!(stdout(), terminal::Clear(terminal::ClearType::All));
-    for current_row in 0..scrheight {
+    //execute!(stdout(), cursor::MoveTo(cx, cy));
+    for current_row in 0..scrheight - 1 {
         let line_idx = scroll_offset + current_row;
         if line_idx < file.len_lines() {
             print!("{}\r", file.line(line_idx));
         } else {
-            print!("~\r");
+            print!("~\r\n");
         }
     }
     stdout().flush().unwrap();
